159ad7fd48de30336196f74a9a6f260e31240c39
AmaanGodhrawala <79107344+AmaanGodhrawala@users.noreply.github.com>
2022-01-07 23:34:56 +0530

    Enter

--- index.js
+++ index.js
@@ -20,9 +20,10 @@ for (item of buttons) {
             if(screen.value=="666"){
                 document.getElementById("calculator").className="broken";
                 document.getElementById("calculator").className="calcHell";
+                document.getElementById("body").className="hell";
                 // document.getElementsByClassName("td").className="disappear";
                 // document.querySelectorAll("td").className="disappear";
-                document.getElementById("body").className="hell";
+               
             }
             
         }
@@ -37,7 +38,42 @@ for (item of buttons) {
             document.getElementById("calculator").className="calcHell";
             // document.getElementsByTagName("td").className="disappear";
             // document.querySelectorAll("td").className="disappear";
+            let tds= document.getElementsByClassName("td")
+            console.log(tds);
+            for(i=0; i<tds.length; i++){
+                tds[i].className="disappear"
+            }
+            // console.log(tds);
+            let leftouts=document.getElementsByClassName("leftout")
+            console.log(leftouts);
+            for(i=0; i<leftouts.length; i++){
+                leftouts[i].className="disappear"
+            }
+            // dis=document.getElementsByClassName("disappear")
+            // console.log(dis);
 
+            let leftouts1=document.getElementsByClassName("leftout1")
+            console.log(leftouts1);
+            for(i=0; i<leftouts1.length; i++){
+                leftouts1[i].className="disappear"
+            }
+            let leftouts2=document.getElementsByClassName("leftout2")
+            console.log(leftouts2);
+            for(i=0; i<leftouts2.length; i++){
+                leftouts2[i].className="disappear"
+            }
+            let leftouts3=document.getElementsByClassName("leftout3")
+            console.log(leftouts3);
+            for(i=0; i<leftouts3.length; i++){
+                leftouts3[i].className="disappear"
+            }
+            document.getElementById("marquee").className="disappear"
+            const music=new Audio("hell.mp3");
+            music.play();
+            music.loop=true;
+            // music.playbackRate=2; 
+            
+// tds.className("disappear")
 
 
 
