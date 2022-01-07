47b0286765263642a14140ad40baf6bf10eaeaee
AmaanGodhrawala <79107344+AmaanGodhrawala@users.noreply.github.com>
2022-01-07 21:12:34 +0530

    Enter

--- index.js
+++ index.js
@@ -13,15 +13,37 @@ for (item of buttons) {
         }
         else if (buttonText == "C") {
             screenValue = "";
-            screen.value = screenValue;
+            screen.value=screenValue;          
         }
         else if (buttonText == "=") {
             screen.value = eval(screenValue);
+            if(screen.value=="666"){
+                document.getElementById("calculator").className="broken";
+                document.getElementById("calculator").className="calcHell";
+                // document.getElementsByClassName("td").className="disappear";
+                // document.querySelectorAll("td").className="disappear";
+                document.getElementById("body").className="hell";
+            }
+            
         }
         else {
             screenValue += buttonText;
             screen.value = screenValue;
         }
 
+        if (screenValue == "666"){
+            document.getElementById("calculator").className="broken";
+            document.getElementById("body").className="hell";
+            document.getElementById("calculator").className="calcHell";
+            // document.getElementsByTagName("td").className="disappear";
+            // document.querySelectorAll("td").className="disappear";
+
+
+
+
+
+
+        }
+            
     });
 }
